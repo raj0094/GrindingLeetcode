e5a3cdd4c7ab63b626b639138f3e6397b51642e3
Rajkishor kumar <rajkishor00094@gmail.com>
2026-08-20 18:09:27 +0530

    Time: 2 ms (19.23%), Space: 46.8 MB (62.51%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -15,31 +15,49 @@
  */
 class Solution {
 
-     public void nthLevel(TreeNode root , int level, int lvl, List<Integer> arr ){
-      if (root == null) return;
-      if(level > lvl) return ;
-      if(level==lvl ) arr.add(root.val); // when level is equal to the given level then it print
-      nthLevel(root.left,level+1,lvl,arr);
-       nthLevel(root.right,level+1,lvl,arr);
-     
+    public class Pair{
+        TreeNode node;
+        int level;
+        Pair(TreeNode node,int level){
+            this.node = node;
+            this.level = level;
+        }
 
-   }
+    }
 
     public int levels(TreeNode root){
         if(root == null) return 0;
         return 1 + Math.max(levels(root.left),levels(root.right));
     }
-    public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> ans = new ArrayList<>();
-        int lvl = levels(root);
-        for(int i =0 ;i<=lvl-1;i++){
-            List<Integer> arr = new ArrayList<>(); 
-            nthLevel(root,0,i,arr);
-            ans.add(arr);
 
+    public void nthudingqueue(TreeNode root,List<List<Integer>> ans) {
+      Queue<Pair> q1 = new LinkedList<>();
+      Pair p  = new Pair(root, 0);
+      if(root != null) q1.add(p);
+      while (q1.size()>0) {
+         Pair front = q1.remove();
+         TreeNode temp = front.node;
+         int lvl = front.level;
 
+         ans.get(lvl).add(temp.val);
+         
+         if (temp.left != null)
+            q1.add(new Pair(temp.left, lvl+1));
+         if (temp.right != null)
+            q1.add(new Pair(temp.right, lvl+1));
         }
+    }
+   
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        int lvl = levels(root);
+        List<List<Integer>> ans = new ArrayList<>();
+        for(int i =1; i<=lvl;i++){
+            List<Integer> arr = new ArrayList<>();  
+            ans.add(arr);
+        }
+        nthudingqueue(root,ans);
         return ans;
+
         
     }
 }
