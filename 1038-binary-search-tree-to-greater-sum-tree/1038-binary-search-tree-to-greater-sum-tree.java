b5a71a3fe3ea42a6849038594d84f2dd8a6d3b52
Rajkishor kumar <rajkishor00094@gmail.com>
2026-08-28 00:29:29 +0530

    Time: 0 ms (100%), Space: 42.9 MB (75.37%) - LeetHub

--- 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.java
+++ 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.java
@@ -14,20 +14,19 @@
  * }
  */
 class Solution {
-    public void inorder(TreeNode root,List<TreeNode> arr){
+    static int sum =0;
+    public void reverseinorder(TreeNode root){
         if(root == null) return;
-        inorder(root.left,arr);
-        arr.add(root);
-        inorder(root.right,arr);
+       reverseinorder(root.right);
+        root.val += sum;
+        sum = root.val;
+        reverseinorder(root.left);
     }
     public TreeNode bstToGst(TreeNode root) {
-        List<TreeNode> arr = new ArrayList<>();
-        inorder(root,arr);
-        int n = arr.size();
-        for(int i =n-2 ;i >=0 ;i--){
-            arr.get(i).val += arr.get(i+1).val;  // get(i) is a node;
-        }
+        sum =0 ;
+        reverseinorder(root);
         return root;
+       
         
     }
 }
