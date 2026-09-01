1630bb835d4ca6c943950be9d874fcfd85d3e093
Rajkishor kumar <rajkishor00094@gmail.com>
2026-09-01 18:45:32 +0530

    Time: 0 ms (100%), Space: 43.6 MB (93.94%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
@@ -1,36 +1,40 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+
 class Solution {
-
-    public void  preorder(TreeNode root,List<TreeNode> ans){
-
-        if(root == null) return ;
-        ans.add(root);
-        preorder(root.left,ans);
-        preorder(root.right,ans);
-    }
     public void flatten(TreeNode root) {
-        List<TreeNode>ans = new ArrayList<>();
-        preorder(root,ans);
-        for(int i = 0; i< ans.size();i++){
-            if(i!= ans.size()-1){
-                ans.get(i).right = ans.get(i+1);
-                ans.get(i).left = null;
+        // break the tree;
+        if(root == null) return;
+        if(root.left == null && root.right == null ) return ;
+        TreeNode LST = root.left;
+        TreeNode RST = root.right;
+
+
+        // flatten the LST and RST 
+        flatten(LST);
+        flatten(RST);
+
+        // arrange in correct order
+        root.right = LST;
+        root.left = null;
+       
+        if(LST != null){    // if LST is  null
+             TreeNode temp = LST;
+             while(temp.right!=null){
+                temp = temp.right;
             }
-           
-        }     
+            temp.right = RST;
+        }else{
+            root.right = RST;
+        }
+
+        
+       
+
+        
+
+
+
+
+        
+       
     }
 }
