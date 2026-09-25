234429da12f9df051c458190dc30410933ec16ba
Rajkishor kumar <rajkishor00094@gmail.com>
2026-09-25 10:43:39 +0530

    Time: 3 ms (97.62%), Space: 84.7 MB (44.8%) - LeetHub

---
+++ 1109-corporate-flight-bookings/1109-corporate-flight-bookings.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] corpFlightBookings(int[][]arr, int n) {
+        int[] ans = new int[n];
+        for(int i = 0;i<arr.length;i++){
+            int first = arr[i][0];
+            int last = arr[i][1];
+            int seats = arr[i][2];
+            ans[first-1] += seats;
+            if(last<n){
+                ans[last+1-1] -= seats;
+            }
+
+        }
+        for(int i =1;i<n;i++){
+            ans[i] = ans[i]+ans[i-1];
+        }
+        return ans;
+        
+    }
+}
